14542d0c5586c0ce4a4a666ed6c37c5412ba5e33
Ygor Magalhaes <emaildoygor@gmail.com>
2020-09-01 18:00:20 -0300

    WIP

--- Collections/Max min/src/Task.kt
+++ Collections/Max min/src/Task.kt
@@ -1,7 +1,15 @@
 // Return a customer who has placed the maximum amount of orders
 fun Shop.getCustomerWithMaxOrders(): Customer? =
-        TODO()
+        this.customers.maxBy { it.orders.size }
 
 // Return the most expensive product that has been ordered by the given customer
 fun getMostExpensiveProductBy(customer: Customer): Product? =
-        TODO()
+        customer.orders
+                .flatMap(Order::products)
+                .maxBy(Product::price)
+
+        /* Original solution
+        customer.orders
+                .mapNotNull { it.products.maxBy { p -> p.price } }
+                .maxBy { it.price }
+         */
