f333a40258d5bd96e064787545b8dc456bce5d00
Ygor Magalhaes <emaildoygor@gmail.com>
2020-09-04 16:52:56 -0300

    Refactoring with callable refs

--- Collections/Compound tasks/src/Task.kt
+++ Collections/Compound tasks/src/Task.kt
@@ -1,20 +1,17 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
-fun findMostExpensiveProductBy(customer: Customer): Product? {
-    return customer.orders.mapNotNull {
-        when (it.isDelivered) {
-            true -> it.products.maxBy(Product::price)
-            else -> null
-        }
-    }.maxBy { it.price }
-}
+fun findMostExpensiveProductBy(customer: Customer): Product? =
+        customer.orders
+                .filter(Order::isDelivered)
+                .flatMap(Order::products)
+                .maxBy(Product::price)
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int =
-    customers.fold(0) { totalOrders, customer ->
-        totalOrders + customer.getOrderedProducts().filter { it == product }.size
-    }
+        customers.fold(0) { totalOrders, customer ->
+            totalOrders + customer.getOrderedProducts().filter { it == product }.size
+        }
 
 
 fun Customer.getOrderedProducts(): List<Product> =
