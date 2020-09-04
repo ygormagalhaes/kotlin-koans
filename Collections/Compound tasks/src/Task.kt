f660f0fa72d24a845644daf475f391866ba7ce8d
Ygor Magalhaes <emaildoygor@gmail.com>
2020-09-04 16:49:46 -0300

    WIP

--- Collections/Compound tasks/src/Task.kt
+++ Collections/Compound tasks/src/Task.kt
@@ -1,14 +1,21 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
 fun findMostExpensiveProductBy(customer: Customer): Product? {
-    TODO()
+    return customer.orders.mapNotNull {
+        when (it.isDelivered) {
+            true -> it.products.maxBy(Product::price)
+            else -> null
+        }
+    }.maxBy { it.price }
 }
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
-fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
-}
+fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int =
+    customers.fold(0) { totalOrders, customer ->
+        totalOrders + customer.getOrderedProducts().filter { it == product }.size
+    }
+
 
 fun Customer.getOrderedProducts(): List<Product> =
-        /* TODO */
+        orders.flatMap(Order::products)
